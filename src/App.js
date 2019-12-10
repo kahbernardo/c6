df29e22292cfb9b740ebadc762e1eb8e88b83281
kahbernardo <kaiquebernardo02@gmail.com>
2019-12-10 00:49:35 -0300

    doidera

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import twitter from './images/twitter.jpg'
 import linkedin from './images/linkedin.png'
 import google from './images/google.png'
 import email from './images/email.png'
+import carbon from './images/textura.jpg'
 
 function App() {
   return (
@@ -22,7 +23,11 @@ function App() {
       </div>
 
       <div className="corpo">
+        <div className='degrade'>
+          <img src={carbon}/>
+        </div>
         <span className="titulo">Por que C6 Bank?
+          <br/>
             <span className="subtitulo">
               O C é de carbono, elemento base da vida, e 6 é o número atômico desse elemento. Os átomos do carbono se unem de várias formas e são capazes de criar diferentes substâncias, desde o grafite, em uma combinação simples, até a mais rara, como o diamante. A habilidade do carbono de fazer conexões nos inspira.
             </span>
